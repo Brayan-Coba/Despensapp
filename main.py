9a5b9df297eccec92043e743099b370813d97cd1
Brayan-Coba <59899727+Brayan-Coba@users.noreply.github.com>
2020-12-17 12:15:11 -0500

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,16 @@ from fastapi import FastAPI, HTTPException
 
 api = FastAPI()
 
+from fastapi.middleware.cors import CORSMiddleware
+origins = [
+    "http://localhost.tiangolo.com", "https://localhost.tiangolo.com",
+    "http://localhost", "http://localhost:8080",
+]
+api.add_middleware(
+    CORSMiddleware, allow_origins=origins,
+    allow_credentials=True, allow_methods=["*"], allow_headers=["*"],
+)
+
 @api.get("/inventario/{item}")
 async def get_item(item: str):
 
